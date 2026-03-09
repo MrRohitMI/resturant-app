a3c55680c9387ffc4b4615078eb9e9d9f77595b4
Rohit Mourya <rohit.mourya@ilensys.com>
2026-03-09 06:59:30 +0530

    * Added all testing configuration ex jest, react testing library and babel * write some basic mock tests * added the environment for tests

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,11 @@ const Contact = () => {
   return (
     <>
       <h1>ContactUs</h1>
-      <h3>This is Contact Page.</h3>
+      <div>
+        <input type="text" placeholder="Enter Name" className="border border-black p-2 m-2" />
+        <input type="text" placeholder="Enter Description" className="border border-black p-2 m-2"/>
+        <button className="border border-gray-500 bg-gray-300 rounded p-2 m-2">Submit</button>
+      </div>
     </>
   );
 };
---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
+
+test('Contact is rendering or not', () => {
+    render(<Contact/>)
+
+    const heading = screen.getByRole("heading");
+
+    expect(heading).toBeInTheDocument();
+})
+
+test('Button is available in contact page', () => {
+    render(<Contact/>)
+
+    const button = screen.getByRole("button");
+
+    expect(button).toBeInTheDocument();
+})
+
+test('Input Name is available or nor', () => {
+    render(<Contact/>)
+
+    const inputName = screen.getByPlaceholderText("Enter Name");
+
+    expect(inputName).toBeInTheDocument();
+})
+
+test('Should load 2 input in contact page', () => {
+    render(<Contact/>)
+
+    // Querying
+    const inputBoxes = screen.getAllByRole('textbox');
+    // Assert
+    expect(inputBoxes.length).toBe(2);
+})
